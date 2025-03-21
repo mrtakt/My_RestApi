7957d3624a97d8115225ccb042b67a72fa2b2628
Yuki <145704515+MrTakt@users.noreply.github.com>
2025-03-21 20:10:35 +0000

    DELETE

--- model/DataWeb.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const dataweb = mongoose.Schema({
-    RequestToday: { type: Number }
-}, { versionKey: false });
-module.exports = mongoose.model('Website Database', dataweb);
