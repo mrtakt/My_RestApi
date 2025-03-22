b7683b420a9f24a144da43573ac17dd4d1e71acc
Yuki <145704515+MrTakt@users.noreply.github.com>
2025-03-22 23:21:08 +0000

    kunyuk

--- routes/admin.js
+++ routes/admin.js
@@ -31,68 +31,13 @@ let getinfo =  await getApikey(req.user.id)
 let { apikey, username, checklimit, isVerified , RequestToday , email,role } = getinfo
 res.render("admin/admin", { username: username, verified: isVerified, apikey: apikey, limit: checklimit , RequestToday: RequestToday , email: email ,role: role});
 });
-router.get('/admin/userdata', checkRole('admin'), async (req, res) => {
+router.get('/admin/userdata', async (req, res) => {
     let getinfo =  await getApikey(req.user.id)
     let { apikey, username, checklimit, isVerified , RequestToday , email,role } = getinfo
     res.render("admin/datauser", { username: username, verified: isVerified, apikey: apikey, limit: checklimit , RequestToday: RequestToday , email: email ,role: role});
     });
 //_______________________ ┏ Router CRUD ┓ _______________________\\
-// Rute untuk menampilkan data
-router.get('/admin/users', async (req, res) => {
-    try {
-        const users = await User.find(); // Mengambil semua pengguna dari database
-        res.render('users', { users }); // Mengirim data pengguna ke template EJS
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-// Rute untuk menampilkan form tambah pengguna
-router.get('/admin/users/new', (req, res) => {
-    res.render('newUser ');
-});
 
-// Rute untuk menyimpan pengguna baru
-router.post('/admin/users', async (req, res) => {
-    const { username, email, apikey,limitApikey,role } = req.body;
-    const newUser  = new User({ username, email, apikey,limitApikey,role });
-    try {
-        await newUser .save();
-        res.redirect('/users'); // Arahkan kembali ke daftar pengguna
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-// Rute untuk menampilkan form edit pengguna
-router.get('/admin/users/edit/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        res.render('editUser ', { user });
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-// Rute untuk memperbarui pengguna
-router.post('/admin/users/edit/:id', async (req, res) => {
-    const { username, email, apikey,limitApikey,role } = req.body;
-    try {
-        await User.findByIdAndUpdate(req.params.id, { username, email, apikey,limitApikey,role });
-        res.redirect('/users'); // Arahkan kembali ke daftar pengguna
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-// Rute untuk menghapus pengguna
-router.post('/admin/users/delete/:id', async (req, res) => {
-    try {
-        await User.findByIdAndDelete(req.params.id);
-        res.redirect('/users'); // Arahkan kembali ke daftar pengguna
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
 //_______________________ ┏ END ┓ _______________________\\
 
   module.exports = router;
