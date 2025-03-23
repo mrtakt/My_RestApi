9a15d8fa919751ea4d9fc76a773365aecd8d5005
Yuki <145704515+MrTakt@users.noreply.github.com>
2025-03-23 11:40:23 +0000

    update crud

--- routes/admin.js
+++ routes/admin.js
@@ -1,43 +1,67 @@
-__path = process.cwd()
-//―――――――――――――――――――――――――――――――――――――――――― ┏  Modules ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
+__path = process.cwd();
+//―――――――――――――――――――――――――――――――――――――――――― ┏ Modules ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 
-require('../settings');
-const express = require('express');
+require("../settings");
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-require('../controller/passportLocal')(passport);
-const dataweb = require('../model/DataWeb');
-const User = require('../model/user');
+const passport = require("passport");
+require("../controller/passportLocal")(passport);
+const dataweb = require("../model/DataWeb");
+const User = require("../model/user");
+const bcryptjs = require("bcryptjs");
 //_______________________ ┏ Function ┓ _______________________\\
 function checkRole(role) {
-    return function(req, res, next) {
-        if (req.isAuthenticated() && req.user.role === role) {
-            return next(); // Jika pengguna terautentikasi dan memiliki peran yang sesuai, lanjutkan ke rute berikutnya
-        }
-          res.redirect('/dashboard');
-    };
-  }
+  return function (req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next(); // Jika pengguna terautentikasi dan memiliki peran yang sesuai, lanjutkan ke rute berikutnya
+    }
+    res.redirect("/dashboard");
+  };
+}
 async function getApikey(id) {
-    let limit = await dataweb.findOne();
-    let users = await User.findOne({_id: id})
-    return {apikey: users.apikey, username: users.username, checklimit: users.limitApikey, isVerified : users.isVerified, RequestToday: limit.RequestToday , email: users.email};
+  let limit = await dataweb.findOne();
+  let users = await User.findOne({ _id: id });
+  return {
+    apikey: users.apikey,
+    username: users.username,
+    checklimit: users.limitApikey,
+    isVerified: users.isVerified,
+    RequestToday: limit.RequestToday,
+    email: users.email,
+  };
 }
 //_______________________ ┏ Router GET?POST ┓ _______________________\\
 
-
-
-router.get('/admin', checkRole('admin'), async (req, res) => {
-let getinfo =  await getApikey(req.user.id)
-let { apikey, username, checklimit, isVerified , RequestToday , email,role } = getinfo
-res.render("admin/admin", { username: username, verified: isVerified, apikey: apikey, limit: checklimit , RequestToday: RequestToday , email: email ,role: role});
+router.get("/admin", checkRole("admin"), async (req, res) => {
+  let getinfo = await getApikey(req.user.id);
+  let { apikey, username, checklimit, isVerified, RequestToday, email, role } =
+    getinfo;
+  res.render("admin/admin", {
+    username: username,
+    verified: isVerified,
+    apikey: apikey,
+    limit: checklimit,
+    RequestToday: RequestToday,
+    email: email,
+    role: role,
+  });
 });
-router.get('/admin/userdata', async (req, res) => {
-    let getinfo =  await getApikey(req.user.id)
-    let { apikey, username, checklimit, isVerified , RequestToday , email,role } = getinfo
-    res.render("admin/datauser", { username: username, verified: isVerified, apikey: apikey, limit: checklimit , RequestToday: RequestToday , email: email ,role: role});
-    });
+
 //_______________________ ┏ Router CRUD ┓ _______________________\\
+router.get("/dataku", checkRole("admin"), async (req, res) => {
+  let getinfo = await getApikey(req.user.id);
+  const datas = await User.find();
+  let { username, isVerified, email, role } = getinfo;
+  res.render("admin/datalist", {
+    username: username,
+    verified: isVerified,
+    email: email,
+    role: role,
+    items: datas,
+  });
+});
+
 
 //_______________________ ┏ END ┓ _______________________\\
 
-  module.exports = router;
+module.exports = router;
